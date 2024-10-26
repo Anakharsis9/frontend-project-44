b6d5460982d474463befff0cd953106dcb10a129
Ismailova Dayana <dayana530807@gmail.com>
2024-10-26 19:34:06 +0500

    feat: add brain-gcd game

---
+++ src/games/brain-gcd.js
@@ -0,0 +1,27 @@
+import { playGame } from "../index.js";
+import { generateNumber } from "../utils.js";
+
+const generateQuestion = () => {
+  const num1 = generateNumber(100);
+  const num2 = generateNumber(100);
+
+  return `${num1} ${num2}`;
+};
+
+const findGCD = numbers => {
+  let [a, b] = numbers.split(" ").map(num => Number(num));
+
+  while (b !== 0) {
+    const temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return String(a);
+};
+
+export const gcdGame = () =>
+  playGame(
+    "Find the greatest common divisor of given numbers.",
+    generateQuestion,
+    findGCD
+  );
