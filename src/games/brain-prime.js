1351b3a5044fd52070c4c4585e775af6f806f437
Ismailova Dayana <dayana530807@gmail.com>
2024-10-26 21:54:22 +0500

    feat: add brain-prime game

---
+++ src/games/brain-prime.js
@@ -0,0 +1,19 @@
+import { playGame } from "../index.js";
+import { generateNumber } from "../utils.js";
+
+const isPrime = number => {
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return "no";
+    }
+  }
+
+  return "yes";
+};
+
+export const primeGame = () =>
+  playGame(
+    "Answer 'yes' if given number is prime. Otherwise answer 'no'.",
+    () => generateNumber(1, 100),
+    isPrime
+  );
