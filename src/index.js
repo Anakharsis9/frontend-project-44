ac7355694eee5e56868915522e12991c5f84e2a9
Ismailova Dayana <dayana530807@gmail.com>
2024-10-31 22:12:24 +0500

    refactor(index.js): remove import cli.js, generateRound, change playGame to runGame

--- src/index.js
+++ src/index.js
@@ -1,38 +1,34 @@
 import readlineSync from 'readline-sync';
-import { greeting, tryAgain, congratulations } from './cli.js';
 
-// Функция для проведения одного раунда игры
-const playRound = (generateQuestion, getCorrectAnswer) => {
-  const question = generateQuestion();
-  const correctAnswer = getCorrectAnswer(question);
+const maxCorrectAnswers = 3;
 
-  console.log(`Question: ${question}`);
-  const userAnswer = readlineSync.question('Your answer: ');
+const runGame = (description, generateQuestion) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 
-  if (userAnswer === correctAnswer) {
-    console.log('Correct!');
-    return true; // Ответ правильный
-  }
-  console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-  tryAgain();
-  return false; // Ответ неправильный
-};
-
-// Основная функция игры
-export default function playGame(description, generateQuestion, getCorrectAnswer) {
-  greeting();
   console.log(description);
 
   let correctAnswersCount = 0;
-  const maxCorrectAnswers = 3;
 
   while (correctAnswersCount < maxCorrectAnswers) {
-    const isCorrect = playRound(generateQuestion, getCorrectAnswer);
-    if (!isCorrect) {
-      return; // Завершаем игру при неправильном ответе
+    const [question, correctAnswer] = generateQuestion();
+
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+
+      return;
     }
+
+    console.log('Correct!');
     correctAnswersCount += 1;
   }
 
-  congratulations();
-}
+  console.log(`Congratulations, ${name}!`);
+};
+
+export default runGame;
