b21be9bdfd86ec0d3e42b974639314b0a0bad1ac
Ismailova Dayana <dayana530807@gmail.com>
2024-10-31 22:09:18 +0500

    refactor(cli.js): export only greet()

--- src/cli.js
+++ src/cli.js
@@ -1,20 +1,9 @@
 import readlineSync from 'readline-sync';
 
-let name = '';
-
-export function greeting() {
+const greet = () => {
   console.log('Welcome to the Brain Games!');
-  name = readlineSync.question('May I have your name? ');
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-}
+};
 
-export function getUserName() {
-  return name;
-}
-
-export function congratulations() {
-  console.log(`Congratulations, ${name}!`);
-}
-export function tryAgain() {
-  console.log(`Let's try again, ${name}!`);
-}
+export default greet;
