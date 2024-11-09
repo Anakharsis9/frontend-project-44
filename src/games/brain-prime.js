5fd9baf0659efc8b697bdf7966ae58cba2faff1a
Ismailova Dayana <dayana530807@gmail.com>
2024-11-09 20:36:38 +0500

    refactor(brain-prime): clear func isPrime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -4,20 +4,20 @@ import { getRandomNumber } from '../utils.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
-  if (number <= 1) return 'no';
+  if (number <= 1) return false;
 
   for (let i = 2; i <= Math.sqrt(number); i += 1) {
     if (number % i === 0) {
-      return 'no';
+      return false;
     }
   }
 
-  return 'yes';
+  return true;
 };
 
 const generateRound = () => {
   const number = getRandomNumber(1, 100);
-  const answer = isPrime(number);
+  const answer = isPrime(number) ? 'yes' : 'no';
   return [number, answer];
 };
 
