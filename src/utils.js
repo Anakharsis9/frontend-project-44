023976994eeda9938e9e67e583164a5c1ab3ccff
Ismailova Dayana <dayana530807@gmail.com>
2024-11-09 20:11:48 +0500

    feat: add getRandomIndex, refactor name getRandomIndex

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
-export default function generateNumber(min = 0, max = 10) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+export const getRandomNumber = (min = 0, max = 10) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+export const getRandomIndex = (collection) => Math.floor(Math.random() * collection.length);
