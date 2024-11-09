ea4506b74d1a81c66a14e0dd04b2d3b7fee9b4eb
Ismailova Dayana <dayana530807@gmail.com>
2024-11-09 20:17:04 +0500

    refactor(brain-even): remove redundant function getCorrectAnswer

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -5,11 +5,9 @@ const description = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 const isEven = (number) => number % 2 === 0;
 
-const getCorrectAnswer = (number) => (isEven(number) ? 'yes' : 'no');
-
 const generateRound = () => {
   const number = getRandomNumber();
-  const answer = getCorrectAnswer(number);
+  const answer = isEven(number) ? 'yes' : 'no';
 
   return [number, answer];
 };
