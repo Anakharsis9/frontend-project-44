cae639d3503dafb976458489bcd91f89e2ccd633
Ismailova Dayana <dayana530807@gmail.com>
2024-11-09 21:06:57 +0500

    chore: remove code max-len change

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line max-len
 export const getRandomNumber = (min = 0, max = 10) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 export const getRandomIndex = (collection) => Math.floor(Math.random() * collection.length);
