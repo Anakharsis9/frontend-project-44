ee2fc01632df2b82eff0c1a21187ce4ad1d5f5b0
Ismailova Dayana <dayana530807@gmail.com>
2024-11-09 20:25:19 +0500

    refactor(brain-gcd): remove redundant func

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -3,28 +3,21 @@ import { getRandomNumber } from '../utils.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-const generateNumbersPair = () => {
-  const num1 = getRandomNumber(0, 100);
-  const num2 = getRandomNumber(0, 100);
-
-  return `${num1} ${num2}`;
-};
-
-const findGCD = (numbers) => {
-  let [a, b] = numbers.split(' ').map((num) => Number(num));
-
-  while (b !== 0) {
-    const temp = b;
-    b = a % b;
-    a = temp;
+const findGCD = (num1, num2) => {
+  while (num2 !== 0) {
+    const temp = num2;
+    num2 = num1 % num2;
+    num1 = temp;
   }
-  return String(a);
+  return num1;
 };
 
 const generateRound = () => {
-  const numbers = generateNumbersPair();
-  const answer = findGCD(numbers);
-  return [numbers, answer];
+  const num1 = getRandomNumber(0, 100);
+  const num2 = getRandomNumber(0, 100);
+
+  const answer = findGCD(num1, num2);
+  return [`${num1} ${num2}`, String(answer)];
 };
 
 const runGcd = () => runGame(description, generateRound);
